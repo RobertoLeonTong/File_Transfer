bce3735b4f52c10e55393eba07dd82957853ff3d
RobertoLeonTong <chimwha123@gmail.com>
2016-08-08 16:54:13 -0400

    Add files via upload

---
+++ FTP.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using IniParser;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Text.RegularExpressions;
+using FtpLib;
+using System.Windows.Forms;
+
+public class FTP
+{
+    static string Address;
+    static string UserID;
+    static string Password;
+    static string ExecutablePath;
+    static string SaveTo;
+    static string DestDir;
+    static string configFile;
+
+    public void loadconfig(string Config)
+    {
+        try
+        {
+            IniParser.FileIniDataParser parser = new FileIniDataParser();
+            parser.CommentDelimiter = '#';
+            IniData data = parser.LoadFile(Config);
+
+            //Retrieving variables from config file
+                Address = data["Config"]["Address"];
+                UserID = data["Config"]["UserID"];
+                Password = data["Config"]["Password"];
+                ExecutablePath = data["Config"]["ExecutablePath"];
+                SaveTo = data["Config"]["SaveTo"];
+                DestDir = data["Config"]["DestDir"];
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n\nHost Information\n");
+
+            Console.ResetColor();
+            Console.WriteLine("\tAddress: " + Address + "\n");
+            Console.WriteLine("\tUserID: " + UserID + "\n");
+            Console.WriteLine("\tSave to: " + SaveTo + "\n");
+            Console.WriteLine("\tDestination directory: " + DestDir);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n" + e.Message + "\n");
+        }
+    }
+
+    public FTP(){}
+
+    public void Transferfiles(string [] Files)
+    {
+        int FileCount = 0;
+        int FilePassed = 0;
+
+        try
+        {   //Establishes connection to host
+            using (FtpConnection ftp = new FtpConnection(Address, UserID, Password))
+            {
+                //Opens Connection
+                ftp.Open();
+                ftp.Login();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\nTransfer Information\n");
+
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                //Checks if directory exists
+
+
+                //FtpDirectoryInfo[] dir = ftp.GetDirectories();
+                //Console.WriteLine(dir[0].Name);
+
+                if (ftp.DirectoryExists(DestDir))
+                {
+                    Console.WriteLine("\tDirectory: Found");
+
+                    //Sets current directory to defined directory
+                    ftp.SetCurrentDirectory(DestDir);
+
+                    for (int i = 0; i < Files.Count(); i++)
+                    {
+                        if (Files[i] == null) break;
+                        FileCount++;
+                    }
+
+                    //files in current directory
+                    FtpFileInfo[] dirFiles = ftp.GetFiles();
+                    
+                    Console.WriteLine("\n\tCurrent Files in Destination Directory: " + dirFiles.Length + "\n");
+
+                    bool pass;
+
+                    Console.WriteLine("\tFiles Within The Destination Directory:\n");
+
+                    int x = 1;
+                    if (dirFiles.Length > 0)
+                    {
+                        foreach (var file in dirFiles)
+                        {
+                            Console.WriteLine("\t " + " " + x + ") " + file.Name.Trim());
+                            x++;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\t No Files");
+                    }
+                    Console.WriteLine("\n");
+
+                    //Stores the found files into defined directory
+                    for (int i = 0; i < Files.Count(); i++)
+                    {
+                        pass = true;
+
+                        if (Files[i] == null) break;
+
+                        for(int y = 0; y < dirFiles.Length; y++)
+                        {
+                            Match passing = Regex.Match(dirFiles[y].Name.Trim(), Files[i],RegexOptions.IgnoreCase);
+                            
+                            if (passing.Success) {
+                                pass = false;
+                            }
+                        }
+                        
+                        if(pass == true)
+                        {
+                            //ftp.PutFile(SaveTo + Files[i]);
+                            FilePassed++;
+                        }
+                    }
+
+                    Console.WriteLine("\tFiles Transferred: " + FilePassed + " file(s)\n");
+                    MessageBox.Show("Notification:  " + FileCount + " file(s) have been found on " + DateTime.Now + "\nOnly " + FilePassed + " file(s) were passed.", "Notification");
+                }
+                else
+                {
+                    Console.WriteLine("\tDirectory Not Found");
+                    MessageBox.Show("The Directory was not found.", "Notification");
+                }
+
+                //Closes connection
+                ftp.Close();
+
+                Console.ResetColor();//Resets Colours
+            }
+        }
+        catch (Exception e)
+        {
+            Console.ResetColor();//Resets Colours
+            Console.WriteLine("Message: " + e.StackTrace);
+
+        }
+    }
+
+    //private bool DirectoryExists(string )
+}
---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using IniParser;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Text.RegularExpressions;
+using FtpLib;
+using System.Windows.Forms;
+//Roberto Leon Tong June 23rd 2016
+
+
+namespace FileTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program n = new Program();
+            FTP p = new FTP();
+            try
+            {
+                //Initializes parser
+                IniParser.FileIniDataParser parser = new FileIniDataParser();
+                parser.CommentDelimiter = '#';
+                IniData data = parser.LoadFile("config.ini");
+                int Hours = Int32.Parse(data["Config"]["Hours"]);
+                
+                //Definitions
+                Outlook.Application app = new Outlook.Application();
+                Outlook.NameSpace ns = app.GetNamespace("MAPI");
+                Outlook.MAPIFolder Inbox = ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                DateTime D1 = DateTime.Now.Subtract(new TimeSpan(0, 0, 0));
+                DateTime D2 = DateTime.Now.Subtract(new TimeSpan(Hours, 0, 0));
+                Outlook.Items ItemsA = Inbox.Items.Restrict("[ReceivedTime] >=" + D2.ToString("ddd M/dd/yyyy hh:mm tt") +'"');
+                Outlook.Items Items2 = Inbox.Items;
+                String[] Files = new string[10000];
+                Boolean FilesFound = false;
+                int i = 0;
+                Boolean AllowSecondLoop = true;
+
+                //Retrieves variables from config file
+                string SaveTo = data["Config"]["SaveTo"];
+                string Find1 = data["Config"]["Find1"];
+                string Find2 = data["Config"]["Find2"];
+                string Find3 = data["Config"]["Find3"];
+
+                //Start of program
+                Console.WriteLine("-------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Program Start\n");
+                Console.ResetColor();
+
+                Console.WriteLine("\tFolder Name: {0}", Inbox.Name + "\n");
+                Console.WriteLine("\tNumber of emails within " + Hours + " hours found: {0} \n\n\tFrom Time: {1}\n", ItemsA.Count, D2.ToString("yyyy-MM-dd hh:mm tt"));
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nSubject lines to find\n");
+                Console.ResetColor();
+
+                Console.WriteLine("\t1) " + Find1 + "\n");
+                Console.WriteLine("\t2) " + Find2 + "\n");
+                Console.WriteLine("\t3) " + Find3 + "\n");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nFile(s) Found:\n");
+                Console.ResetColor();
+
+                //Regular expressions
+                Regex regexA = new Regex(Find1, RegexOptions.IgnoreCase);
+                Regex regexB = new Regex(Find2, RegexOptions.IgnoreCase);
+                Regex regexC = new Regex(Find3, RegexOptions.IgnoreCase);
+
+                //Searching all emails within inbox
+
+
+                for (int counter = 1; counter <= ItemsA.Count ; counter++)
+                { 
+                    string x = Inbox.Items[counter].subject.Trim();
+                    Match mA = regexA.Match(x);
+                    Match mB = regexB.Match(x);
+                    Match mC = regexC.Match(x);
+
+                    //Regular expression to find current date in files
+                    Regex regex1 = new Regex("(" + D1.ToString("yyyyMMdd") + ")");
+                    
+                    //Matching regular expressions
+                    if (mA.Success || mB.Success || mC.Success )
+                    {   
+                        Outlook.MailItem mail = Inbox.Items[counter];
+                        string x1 = mail.Attachments[1].FileName.Trim();
+                        Match m1 = regex1.Match(x1);
+
+                        if (m1.Success)
+                        {
+                            Files[i] = mail.Attachments[1].FileName;
+                            
+                            //Saves email attachment
+                            if(Files[i] != null)
+                                mail.Attachments[1].SaveAsFile(SaveTo + mail.Attachments[1].FileName);
+
+                            FilesFound = true;
+                            AllowSecondLoop = false;
+                            i++;
+                        }
+                    }
+
+               }
+
+                for (int counter = Items2.Count; counter >= (Items2.Count - ItemsA.Count) && AllowSecondLoop == true; counter--)
+                {
+                    string x = Inbox.Items[counter].subject.Trim();
+                    Match mA = regexA.Match(x);
+                    Match mB = regexB.Match(x);
+                    Match mC = regexC.Match(x);
+
+                    //Regular expression to find current date in files
+                    Regex regex1 = new Regex("(" + D1.ToString("yyyyMMdd") + ")");
+
+                    //Matching regular expressions
+                    if (mA.Success || mB.Success || mC.Success)
+                    {
+                        Outlook.MailItem mail = Inbox.Items[counter];
+                        string x1 = mail.Attachments[1].FileName.Trim();
+                        Match m1 = regex1.Match(x1);
+
+                        if (m1.Success)
+                        {
+                            Files[i] = mail.Attachments[1].FileName;
+
+                            //Saves email attachment
+                            if (Files[i] != null)
+                                mail.Attachments[1].SaveAsFile(SaveTo + mail.Attachments[1].FileName);
+
+                            FilesFound = true;
+                            i++;
+                        }
+                    }
+
+                }
+
+                //Displays all Files found
+                foreach (string file in Files)
+                    if(file != null)
+                    {
+                        Console.WriteLine("\t"+file.ToString());
+                    }
+
+                //Checks if files are found or not. If not it will not call the connection
+                if (FilesFound == false) MessageBox.Show("No Files Found On " + DateTime.Now, "Notification");
+                else
+                {
+                    p.loadconfig("config.ini");
+                    p.Transferfiles(Files);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nProgram Ended");
+                Console.ResetColor();
+
+                Console.WriteLine("-------------------------------------------------\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                MessageBox.Show("Process Failed!");
+            }
+        }
+
+        
+    }
+}
